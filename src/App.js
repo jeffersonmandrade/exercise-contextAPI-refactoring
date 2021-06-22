67cb002c45ae6b4cd7b9294dff7f5a3179345548
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-06-22 08:08:58 -0300

    cars feito

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react'
 import TrafficSignal from './TrafficSignal';
 import './App.css';
 import Cars from './Cars';
+import {trafficContext} from './context'
+import CarProvider from './context/CarProvider';
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      color: 'red'
+    }
+  }
+  changeSignal = (color) =>{
+    this.setState({color: color})
+  }
 
-function App() {
+
+  render() {
+    const provider = {signalColor: this.state.color, changeSignal: this.changeSignal}
+    console.log(provider)
   return (
     <div className="container">
-      <Cars />
-      <TrafficSignal />
+      <CarProvider>
+      <trafficContext.Provider value ={provider}>
+          <Cars />
+          <TrafficSignal />
+      </trafficContext.Provider>
+      </CarProvider>
     </div>
   );
 }
-
-export default App;
+}
